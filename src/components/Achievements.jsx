0cf2d4b658a6a3aabdecc2a5ce124a5a935ced0b
kathrynhoule <kathrynhoule@torontomu.ca>
2024-07-16 10:29:41 -0400

    adds initial component for achievements

---
+++ src/components/Achievements.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useState } from '@hookstate/core';
+import achievementState from '../state/achievementState';
+
+const Achievements = () => {
+     const achievements = useState(achievementState);
+
+};
