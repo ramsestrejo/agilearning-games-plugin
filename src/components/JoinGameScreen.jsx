07d790023a2ed73a7914d30e3709df5e36f3cefc
kathrynhoule <kathrynhoule@torontomu.ca>
2024-07-27 16:25:06 -0400

    updates joingamescreen to use stylesheet

--- src/components/JoinGameScreen.jsx
+++ src/components/JoinGameScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHookstate } from '@hookstate/core';
 import { gameCodeState } from '../state/gameCodeState';
+import './HomeScreen.css';
 
 const JoinGameScreen = () => {
      const gameCode = useHookstate(gameCodeState);
@@ -8,31 +9,33 @@ const JoinGameScreen = () => {
 
      const handleJoin = () => {
           // placeholder logic for joining a game
-          console.log(`Join Game with code: ${gameCode.get()} and username: ${username}`);
+          console.log(`Join game with a code: ${gameCode.get()} and username: ${username}`);
      };
 
      return (
-     <div>
-       <h1>Join Game</h1>
-       <div>
-         <input
-           type="text"
-           placeholder="Enter a Game Code"
-           value={gameCode.get()}
-           onChange={(e) => gameCode.set(e.target.value)}
-         />
-       </div>
-       <div>
-         <input
-           type="text"
-           placeholder="Enter a Username"
-           value={username}
-           onChange={(e) => setUsername(e.target.value)}
-         />
-       </div>
-       <button onClick={handleJoin}>Join Game</button>
+     <div className="home-screen">
+          <h1>Join a Game</h1>
+          <div className="menu">
+               <div>
+               <input
+                    type="text"
+                    placeholder="Enter a Game Code"
+                    value={gameCode.get()}
+                    onChange={(e) => gameCode.set(e.target.value)}
+               />
+               </div>
+               <div>
+               <input
+                    type="text"
+                    placeholder="Enter a Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+               />
+               </div>
+               <button onClick={handleJoin}>Join Game</button>
+          </div>
      </div>
-   );
- };
+     );
+};
  
- export default JoinGameScreen;
+export default JoinGameScreen;
