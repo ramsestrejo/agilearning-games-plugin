1f3351eb969dc3349e70c8d1dcbd50e183c51678
kathrynhoule <kathrynhoule@torontomu.ca>
2024-07-27 20:13:41 -0400

    updates game screen creation styling; adds more game types to array

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './GameCard.css';
 
 const GameCard = ({ type, onCreateGame }) => {
-  return (
-    <div className="game-type-card">
-      <h2>{type.name}</h2>
-      <p>{type.description}</p>
-      <button onClick={() => onCreateGame(type.id)}>Create {type.name} Game</button>
-    </div>
-  );
+     return (
+     <div className="game-type-card">
+          <h2>{type.name}</h2>
+          <p>{type.description}</p>
+          <button onClick={() => onCreateGame(type.id)}>Create {type.name} Game</button>
+     </div>
+     );
 };
 
 export default GameCard;
--- src/state/gameTypeState.jsx
+++ src/state/gameTypeState.jsx
@@ -3,4 +3,7 @@ import { hookstate } from '@hookstate/core';
 export const gameTypeState = hookstate([
   { id: 'Type1', name: 'Quiz', description: 'Info about the quiz type game here' },
   { id: 'Type2', name: 'Story', description: 'Info about the story type game here' },
+  { id: 'Type3', name: 'Kahoot', description: 'Info about the other type game here' },
+  { id: 'Type4', name: 'Trivia', description: 'Info about the other type game here' },
+  { id: 'Type5', name: 'Puzzle', description: 'Info about the other type game here' }
 ]);
