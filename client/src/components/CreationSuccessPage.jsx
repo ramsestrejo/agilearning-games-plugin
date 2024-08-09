f6cbd6c97d2aef5e4bd61f2f34f7f0bac97343ae
kathrynhoule <kathrynhoule@torontomu.ca>
2024-08-09 17:32:33 -0400

    updates creation success page to start implementing the game codes

--- client/src/components/CreationSuccessPage.jsx
+++ client/src/components/CreationSuccessPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const CreationSuccessPage = () => {
-//     const { gameCode } = useParams();
+const CreationSuccessPage = ({ gameCode }) => {
+    const navigate = useNavigate();
+
+    const handlePlayNow = () => {
+        navigate('/join-game');
+    };
 
     return (
         <div className="success-page">
-            <h1>Quiz Successfully Created !</h1>
-            {/* <p>Your game code is: {gameCode}</p> */}
+            <h3>Your game has successfully been created!</h3>
+            <p>Your game code is: {gameCode}</p>
             <p>You can share this code with others to let them take your quiz.</p>
+            <button onClick={handlePlayNow}>Play Now</button>
+            {/* maybe add an option to return to the main menu or create another quiz */}
         </div>
     );
 };
