8e867a57d73505248a1a8a19603b0339606b2733
kathrynhoule <kathrynhoule@torontomu.ca>
2024-08-09 13:59:53 -0400

    updates leaderboard to sort by score

--- client/src/components/QuizGame/Leaderboard.jsx
+++ client/src/components/QuizGame/Leaderboard.jsx
@@ -5,24 +5,26 @@ import React from 'react';
 const Leaderboard = ({ score, resetGame }) => {
      // temporary data until connection to backend
      const leaderboardData = [
-       { playerName: 'Player 1', score: 55 },
-       { playerName: 'Player 2', score: 45 },
+       { playerName: 'Player 1', score: 80 },
+       { playerName: 'Player 2', score: 130 },
        { playerName: 'Player 3', score: 40 },
        { playerName: 'Player 4', score: 30 },
-       { playerName: 'Player 5', score: 25 },
-       { playerName: 'Player 6', score: 15 },
-       { playerName: 'Player 7', score: 10 },
+       { playerName: 'Player 5', score: 10 },
+       { playerName: 'Player 6', score: 20 },
+       { playerName: 'Player 7', score: 40 },
      ];
+
+     const sortedLeaderboardData = [...leaderboardData].sort((a, b) => b.score - a.score);
 
      return (
           <div className="leaderboard">
             <h1>Leaderboard</h1>
             <ul>
-              {leaderboardData.map((entry, index) => (
-                <li key={index}>
-                  {entry.playerName}: {entry.score}
-                </li>
-              ))}
+            {sortedLeaderboardData.map((entry, index) => (
+                    <li key={index}>
+                        {entry.playerName}: {entry.score}
+                    </li>
+                ))}
             </ul>
             <button onClick={resetGame} className="restart-button">Retry Quiz</button>
             {/* Include button option to return to main menu */}
