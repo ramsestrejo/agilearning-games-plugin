b77bdf49ea2d30691644d85d405618709a58d059
Toby Portelli <tobyportelli@gmail.com>
2024-08-10 22:55:04 -0400

    Updates migrations

--- server/migrations/sqls/20240807210757-setup-story-pages-up.sql
+++ server/migrations/sqls/20240807210757-setup-story-pages-up.sql
@@ -3,11 +3,12 @@ CREATE TABLE story_pages (
     story_id INT,
     page_number INT,
     content TEXT NOT NULL,
+    question TEXT NOT NULL,
     FOREIGN KEY (story_id) REFERENCES stories(id) ON DELETE CASCADE
 );
 
-INSERT INTO story_pages (story_id, page_number, content)
+INSERT INTO story_pages (story_id, page_number, content, question)
 VALUES 
-(1, 1, "Piece of story text 1"),
-(1, 2, "Piece of story text 2"),
-(1, 3, "Piece of story text 3");
+(1, 1, "Piece of story text 1", "question 1"),
+(1, 2, "Piece of story text 2", "question 2"),
+(1, 3, "Piece of story text 3", "question 3");
--- server/migrations/sqls/20240807210816-setup-story-answers-up.sql
+++ server/migrations/sqls/20240807210816-setup-story-answers-up.sql
@@ -1,16 +1,18 @@
 CREATE TABLE story_answers (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    question_id INT,
+    story_page_id INT,
+    story_id INT,
     answer_text TEXT,
     is_correct BOOLEAN DEFAULT false,
-    FOREIGN KEY (question_id) REFERENCES story_questions(id) ON DELETE CASCADE
+    FOREIGN KEY (story_page_id) REFERENCES story_pages(id) ON DELETE CASCADE,
+    FOREIGN KEY (story_id) REFERENCES stories(id) ON DELETE CASCADE
 );
 
-INSERT INTO story_answers (question_id, answer_text, is_correct)
+INSERT INTO story_answers (story_page_id, story_id, answer_text, is_correct)
 VALUES 
-(1, "Answer 1", true),
-(1, "Answer 2", false),
-(2, "Answer 1", false),
-(2, "Answer 2", true),
-(3, "yes", true),
-(3, "no", false)
+(1, 1, "Answer 1", true),
+(1, 1, "Answer 2", false),
+(2, 1, "Answer 1", false),
+(2, 1, "Answer 2", true),
+(3, 1, "yes", true),
+(3, 1, "no", false)
