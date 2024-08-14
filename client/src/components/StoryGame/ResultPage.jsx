172e2622ed9a295265ea718f7b52b5e7e51ddc91
kathrynhoule <kathrynhoule@torontomu.ca>
2024-08-14 16:51:51 -0400

    fixing spacing in code lines for games

--- client/src/components/StoryGame/ResultPage.jsx
+++ client/src/components/StoryGame/ResultPage.jsx
@@ -5,30 +5,30 @@ import "./ResultPage.css";
 
 const ResultPage = () => {
      const navigate = useNavigate();
-  const { score } = useParams();
-  const [message, setMessage] = useState("");
+     const { score } = useParams();
+     const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    const fetchCustomMessage = async () => {
-      const response = await fetch(`/api/custom-messages/for-score/${score}`);
-      const result = await response.json();
-      setMessage(result.message);
-    };
+     useEffect(() => {
+          const fetchCustomMessage = async () => {
+          const response = await fetch(`/api/custom-messages/for-score/${score}`);
+          const result = await response.json();
+          setMessage(result.message);
+     };
 
-    fetchCustomMessage();
-  }, [score]);
+     fetchCustomMessage();
+     }, [score]);
 
-  const handleGoHome = () => {
-     navigate("/");
-   };
+     const handleGoHome = () => {
+          navigate("/");
+     };
 
-  return (
-    <div className="result-page">
-      <h1>Your Score: {score}</h1>
-      {message && <p>{message}</p>}
-      <button className="home-button" onClick={handleGoHome}>Home</button>
-    </div>
-  );
+     return (
+          <div className="result-page">
+               <h1>Your Score: {score}</h1>
+               {message && <p>{message}</p>}
+               <button className="home-button" onClick={handleGoHome}>Home</button>
+          </div>
+     );
 };
 
 export default ResultPage;
