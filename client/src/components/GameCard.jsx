a48c4f8a761263c3eb8bd3764aa3c289be62e6f9
kathrynhoule <kathrynhoule@torontomu.ca>
2024-08-13 20:14:25 -0400

    fixed a typo

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -6,7 +6,7 @@ const GameCard = ({ game, onCreateGame }) => {
      <div className="game-type-card">
           <h2>{game.name}</h2>
           <p>{game.description}</p>
-          <button onClick={() => onCreateGame(game.name)}>Create {game.name} Game</button>
+          <button onClick={() => onCreateGame(game.name)}>Create {game.name} game</button>
      </div>
      );
 };
