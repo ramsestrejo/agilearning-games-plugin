30cf630d16ea9ef1827309b2380d4c37162f7dd7
kathrynhoule <kathrynhoule@torontomu.ca>
2024-08-15 12:39:03 -0400

    adds necessary import for state

--- client/src/components/StoryGame/StoryForm.jsx
+++ client/src/components/StoryGame/StoryForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHookstate } from "@hookstate/core";
 import StoryBlockInput from "./StoryBlockInput";
 import "./StoryForm.css";
